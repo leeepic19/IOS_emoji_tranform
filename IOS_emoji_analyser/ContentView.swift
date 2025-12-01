d34b25ace46295713af41c1b681e9641a6883712
leeepic19 <leeepic19@gmail.com>
2025-12-01 13:42:02 +0800

    Initial Commit

---
+++ IOS_emoji_analyser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS_emoji_analyser
+//
+//  Created by 李玉广 on 2025/12/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS_emoji_analyser/IOS_emoji_analyserApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_emoji_analyserApp.swift
+//  IOS_emoji_analyser
+//
+//  Created by 李玉广 on 2025/12/1.
+//
+
+import SwiftUI
+
+@main
+struct IOS_emoji_analyserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
